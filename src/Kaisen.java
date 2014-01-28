39e8ad15972f3c6715eb0493c26413f65937f784
soldier11 <max_tab0_air_plane_039@yahoo.co.jp>
2014-01-29 00:21:52 +0900

    LVに応じた戦闘結果

--- src/Kaisen.java
+++ src/Kaisen.java
@@ -5,42 +5,47 @@ public class Kaisen{
 	
 	public static void main(String[] args) throws java.io.IOException {
 		
-		Greet();      //最初のメッセージ
-		PutCommand(); //選択肢の表示
-		int c = InCommand(); //行動の選択
-				
-		if(c == '1'){
-			System.out.println("あなたの船は破壊されました");
-		}if(c == '2'){
-			System.out.println("あなたの船は被害を免れました");
-			
-		}
+		System.out.println(name + "提督" + ship + "にご命令を！！"); //挨拶
+		Status();             //最初のメッセージ
+		PutCommand();        //選択肢の表示
+		Result();            //勝敗の表示
 	}
 	
-	public static void Greet(){
-		System.out.println(name + "提督" + ship + "にご命令を！！"); //挨拶	
-		System.out.println(ship + "のLVは" + lv + "です。"); //艦のステータス
+	public static void Status(){
+		System.out.println("---------------------------");
+		System.out.println(" " + name + " " + ship + " LV" + lv + " "); //艦のステータス
+		System.out.println("---------------------------");
 	}
 	
-	public static void PutCommand(){
-		System.out.println("行動を決定してください。");
+	public static void PutCommand() throws java.io.IOException{
+		System.out.println("大和への指令を決定してください。");
 		System.out.println("1：出撃");
 		System.out.println("2：演習");
+		int c = InCommand(); //行動の選択
+		
+		if(c == '1'){
+			System.out.println("敵戦艦が現れた。大和の主砲で攻撃します。");
+		}if(c == '2'){
+			lv += 1;
+			System.out.println("演習により"+ ship +"のレベルが"+ lv +"になった。");
+			PutCommand();
+		}
 	}
 	
 	public static int InCommand() throws java.io.IOException{
 		int c = System.in.read(); 
-		System.out.println("入力されたキー番号は" + c + "です。");
-		
-		//改行外し
-		if (c == 10 || c == 13){
-			System.out.println("改行は入力できません");
+		if (c == 10 || c == 13){//改行外し
 			return(InCommand());
 		}else{
 		return(c);
 		}
-		
 	}
 	
-		
+	public static void Result() throws java.io.IOException {
+		if(lv > 20){
+			System.out.println("勝利");
+		}else{
+			System.out.println("負け");			
+		}
+	}
 }
